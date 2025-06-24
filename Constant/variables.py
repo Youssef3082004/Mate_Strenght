21c28cbd439dfd6451868ee3d5b2160e5dbd826c
Youssef Mustafa <115989856+youssef969@users.noreply.github.com>
2025-06-25 01:10:58 +0300

    Constant Folder

---
+++ Constant/variables.py
@@ -0,0 +1,13 @@
+from flet import * 
+
+
+
+
+class Variables:
+    LINEARGRADIENT = LinearGradient(colors=["#B99A45","#1A1A1A",],stops=[0,0.90],begin=alignment.top_right,end=alignment.top_left)
+    BLACK = "#1A1A1A"
+    TAN = "#B99A45"
+    space15 = Container(height=15)
+    Screen_width = Page.width
+    Screen_height = Page.height
+    Null = Text(" ")
