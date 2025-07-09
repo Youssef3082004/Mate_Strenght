5a0ee58b7742af99eaf96cf6833a0b2419e8504c
Youssef Mustafa <115989856+youssef969@users.noreply.github.com>
2025-07-10 02:10:34 +0300

    New Constant Folder

--- Constant/variables.py
+++ Constant/variables.py
@@ -7,6 +7,7 @@ class Variables:
     LINEARGRADIENT = LinearGradient(colors=["#B99A45","#1A1A1A",],stops=[0,0.90],begin=alignment.top_right,end=alignment.top_left)
     BLACK = "#1A1A1A"
     TAN = "#B99A45"
+    PAGES_BACKGROUND_COLOR = "#f1f5f8"
     space15 = Container(height=15)
     Screen_width = Page.width
     Screen_height = Page.height
